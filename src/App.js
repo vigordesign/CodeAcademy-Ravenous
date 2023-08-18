8052cb80c5ec792c0c2a53ba13175870744b50e4
Kevin Pelletier <vigordesign@gmail.com>
2023-08-18 16:52:27 -0400

    Built the Business Component!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Business from './components/Business';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. HONK! aaaaaaaaaa
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Business/>
     </div>
   );
 }
