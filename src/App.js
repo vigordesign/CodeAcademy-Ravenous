46968d2d9560935a63bcffe94b89c8c7622c5d40
Kevin Pelletier <vigordesign@gmail.com>
2023-08-28 14:16:58 -0400

    Finished part 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import './App.css';
 import BusinessList from "./components/BusinessList/BusinessList";
 import SearchBar from './components/SearchBar/SearchBar';
 
+const business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Bordertown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+}
+
+const businesses = [business, business, business, business, business, business];
+
 const App = () => {
   return (
     <div className="App">
@@ -10,7 +25,7 @@ const App = () => {
         <h2>Ravenous</h2>
       </div>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,19 +2,14 @@ import React from "react";
 import styles from "./BusinessList.css";
 import Business from "../Business/Business";
 
-const BusinessList = () => {
+const BusinessList = ({businesses}) => {
     return(
         <div className="bizList">
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
+            {businesses.map((business) => (
+                <Business business={business} key={business.name}  />
+            ))}
         </div>
     )
 };
 
 export default BusinessList;
-
-
